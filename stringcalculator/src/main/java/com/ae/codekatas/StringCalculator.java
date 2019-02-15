4ffbe5df53712413c0d8c49aa6812682bc0d3cbf
Matthias Van Eeghem <matthias.vaneeghem@vdab.be>
2019-02-15 14:40:41 +0100

    [String calculator] Test for getCustomDelimiter

--- stringcalculator/src/main/java/com/ae/codekatas/StringCalculator.java
+++ stringcalculator/src/main/java/com/ae/codekatas/StringCalculator.java
@@ -1,11 +1,16 @@
 package com.ae.codekatas;
 
+import java.util.Optional;
+
 public class StringCalculator {
 
     public static void main(String[] args) {
     }
 
     public static int add(String numbers) {
+        if (numbers.length() == 0)
+            return 0;
+
         int sum = 0;
 
         for(String numberAsString : numbers.split(",|\\n")) {
@@ -14,6 +19,16 @@ public class StringCalculator {
         }
 
         return sum;
+    }
+
+    public static Optional<String> getCustomDelimiter(String input) {
+        if (input.startsWith("//")) {
+            String inputWithoutSlashes = input.substring(2);
+            String customDelimiter = inputWithoutSlashes.split("\n", 2)[0];
+            return Optional.of(customDelimiter);
+        }
+
+        return Optional.empty();
     }
 }
 
--- stringcalculator/src/test/java/com/ae/codekatas/StringCalculatorTest.java
+++ stringcalculator/src/test/java/com/ae/codekatas/StringCalculatorTest.java
@@ -3,26 +3,35 @@ package com.ae.codekatas;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 public class StringCalculatorTest {
 
     @Test
-    public void addEmptyString() {
+    public void addEmptyStringTest() {
         Assertions.assertEquals(0, StringCalculator.add(""));
     }
 
     @Test
-    public void addOneNumber() {
+    public void addOneNumberTest() {
         Assertions.assertEquals(1, StringCalculator.add("1"));
     }
 
     @Test
-    public void addMultipleNumbers() {
+    public void addMultipleNumbersTest() {
         Assertions.assertEquals(348, StringCalculator.add("1,2,345"));
     }
 
     @Test
-    public void addMultipleNumbersWithNewlineAndComma() {
+    public void addMultipleNumbersWithNewlineAndCommaTest() {
         Assertions.assertEquals(348, StringCalculator.add("1\n2,345"));
+    }
+
+    @Test
+    public void customDelimiterTest() {
+        Optional<String> customDelimiter = StringCalculator.getCustomDelimiter("//;\n1;2");
+        Assertions.assertTrue(customDelimiter.isPresent());
+        Assertions.assertEquals(";", customDelimiter.get());
     }
 }
 
